d4afbea75bda31c174611c267d5dbf13ac16e0ae
Cole Busby <colebusby@gmail.com>
2013-09-25 08:26:16 -0600

    removing old deleted files

--- app/models/rating.rb
+++
@@ -1,7 +0,0 @@
-class Rating < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :video, dependent: :destroy
-
-  validates :description, presence: true
-  validates_uniqueness_of :user_id, scope: [ :video_id ]
-end
--- spec/fabricators/rating_fabricator.rb
+++
@@ -1,4 +0,0 @@
-Fabricator(:rating) do
-  rate { rand(1..5) }
-  description { Faker::Lorem.paragraph(2)}
-end
