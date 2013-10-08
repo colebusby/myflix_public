77899b7b3d8ecd9c9a8577e345012ff32d1ca383
Cole Busby <colebusby@gmail.com>
2013-10-08 15:09:03 -0600

    changes

---
+++ spec/features/user_resets_password_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+feature "user resets password" do
+  scenario "user successfully resets the password" do
+    lisa = Fabricate(:user, password: 'old_password')
+    visit signin_path
+    click_link 'Forgot Password?'
+    fill_in 'Email Address', with: lisa.email
+    click_button 'Send Email'
+
+    open_email(lisa.email)
+    current_email.click_link('Reset Password')
+
+    fill_in 'New Password', with: 'new_password'
+    click_button 'Reset Password'
+
+    fill_in 'Email Address', with: lisa.email
+    fill_in 'Password', with: 'new_password'
+    click_button 'Sign in'
+
+    expect(page).to have_content "Welcome, #{lisa.username}"
+  end
+end
