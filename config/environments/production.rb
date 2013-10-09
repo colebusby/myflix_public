54e7e3ef2ee61e2d46649ea44dcb324923aeb0f4
Cole Busby <colebusby@gmail.com>
2013-10-08 19:04:12 -0600

    changes to config for mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,6 @@ Myflix::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'safe-journey-6359.heroku.com' }
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
@@ -60,6 +59,7 @@ Myflix::Application.configure do
     password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
+  config.action_mailer.default_url_options = { host: 'safe-journey-6359.herokuapp.com' }
 
   # Enable threaded mode
   # config.threadsafe!
