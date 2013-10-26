82c1bcbaed25361c9f9250b2d515efd3dbf6050f
Cole Busby <colebusby@gmail.com>
2013-10-26 10:16:25 -0600

    more s3 changes

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,6 +5,7 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['S3_KEY'],                        # required
       :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
+      :region                 => 'us-west-2',                  # optional, defaults to 'us-east-1'
     }
     config.fog_directory  = ENV['MYFLIX_BUCKET']                     # required
   else
